8d3e0bcadd1da73970624247746100d22105d73d
= <anton.shheglov.1@gmail.com>
2020-01-29 17:04:16 +0300

    add: model of Photo.

---
+++ Gallery/Models/Photo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace Gallery.Models
+{
+    [Serializable]
+    public class Photo : IDataErrorInfo
+    {
+        public Photo(string name, string path)
+        {
+            this.Picture = Image.FromFile(path);
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(columnName))
+                {
+                    return "Please, enter correct value";
+                }
+
+                return null;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public Image Picture { get; set; }
+
+        public string Error { get { return null; } }
+    }
+}
