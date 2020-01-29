88f61f588acdc4d7d41aa9e0264f8e66b38c13c2
= <anton.shheglov.1@gmail.com>
2020-01-29 18:27:29 +0300

    feat: blazor creates necessary count of html tags with dynamic css and other.

--- Gallery/Controllers/HomeController.cs
+++ Gallery/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Gallery.Models;
+using Gallery.DAL;
 
 namespace Gallery.Controllers
 {
@@ -10,6 +12,15 @@ namespace Gallery.Controllers
     {
         public ActionResult Index()
         {
+            using (MyContext context = new MyContext())
+            {
+                ViewBag.Photoes = context.Photoes.ToList();
+                context.SaveChanges();
+            }
+
+            Console.WriteLine(ViewBag.Photoes.Count);
+            Console.ReadLine();
+
             return View();
         }
 
@@ -25,6 +36,19 @@ namespace Gallery.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Show()
+        {
+            Photo ph = new Photo("Clouds", @"https://i.artfile.me/wallpaper/06-10-2017/1920x1080/priroda-oblaka-clouds-nebo-sky-1244838.jpg");
+            Photo ph0 = new Photo("Clouds", @"https://www.tokkoro.com/picsup/5767333-clouds-wallpapers.jpg");
+            using (MyContext context = new MyContext())
+            {
+                context.Photoes.Add(ph);
+                context.Photoes.Add(ph0);
+                context.SaveChanges();
+            }
+            return View("Show", ph);
         }
     }
 }
